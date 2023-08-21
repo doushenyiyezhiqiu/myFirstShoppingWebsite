4dd4e92c481ad3300ff60c6c4bba2e61a528ee96
doushenyiyezhiqiu <li.liangs@northeastern.edu>
2023-08-21 17:18:37 -0400

    fix some bugs and add email receipt funciton

--- 03-frontend/angular-ecommerce/src/app/common/payment-info.ts
+++ 03-frontend/angular-ecommerce/src/app/common/payment-info.ts
@@ -1,7 +1,6 @@
 export class PaymentInfo {
 
-    constructor(public amount?: number,
-                public currency?: string) {
-                    
-    }
+    amount: number;
+    currency: string;
+    receiptEmail: string;
 }
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -42,6 +42,8 @@ export class CheckoutComponent implements OnInit{
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
   constructor(private formBuilder: FormBuilder,
               private myShoppingWebsiteFormService: MyShoppingWebsiteFormService,
               private cartService: CartService,
@@ -275,8 +277,9 @@ export class CheckoutComponent implements OnInit{
     purchase.orderItems = orderItems;
 
     // compute payment info
-    this.paymentInfo.amount = this.totalPrice * 100;
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
     this.paymentInfo.currency = "USD";
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     // if valid form then
     // - create payment intent
@@ -285,12 +288,25 @@ export class CheckoutComponent implements OnInit{
 
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
+      this.isDisabled = true;
+
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
             {
               payment_method: {
-                card: this.cardElement
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry.value.code
+                  }
+                }
               }
             }, {handleActions: false})
             .then((result: any) => {
@@ -305,9 +321,11 @@ export class CheckoutComponent implements OnInit{
                   
                    // reset cart
                    this.resetCart();
+                   this.isDisabled = false;
                   },
                   error: (err: any) => {
                     alert(`There was an error: ${err.message}`);
+                    this.isDisabled = false;
                   }
               })
               }
@@ -320,11 +338,13 @@ export class CheckoutComponent implements OnInit{
     }
 
   }
+
   resetCart() {
     // reset cart data
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     // reset the form
     this.checkoutFormGroup.reset();
